135c04fb3aa6232fd5cccbfd43ce908d811fe37d
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-15 10:12:28 +0530

    New project created

---
+++ src/main/java/com/pdtech/talentnet/TalentNetApplication.java
@@ -0,0 +1,13 @@
+package com.pdtech.talentnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentNetApplication.class, args);
+    }
+
+}
