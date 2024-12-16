e256812ecc847b54bc4d4be9e6d94d22ce02b808
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-16 14:24:18 +0530

    All module templates added

---
+++ Jobs/src/test/java/com/pdtech/talentnet/jobs/JobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdtech.talentnet.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Posts/src/test/java/com/pdtech/talentnet/posts/PostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdtech.talentnet.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
